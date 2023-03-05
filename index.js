cf45654a01413fa88e01a4a0c1c5561cf5c6bb83
atran50 <atran50@asu.edu>
2023-03-04 17:51:51 -0700

    Adding function to get input and log to console

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ function changeColor()
         light = true;
     }
 }
+
+function getInput()
+{
+    var init = document.getElementById("initInput");
+    var name = document.getElementById("nameInput");
+    var hp = document.getElementById("hpInput");
+    var notes = document.getElementById("notesInput");
+    console.log(init.value + " " + name.value + " " + hp.value + " " + notes.value);
+
+    init.value="";
+    name.value="";
+    hp.value="";
+    notes.value="";
+}
